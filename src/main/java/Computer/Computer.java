01db5c1afd40fda1419995729ede66bb91424668
Alexander Zeitlhofer <alexander.zeitlhofer@sz-ybbs.ac.at>
2022-06-05 18:55:16 +0200

    !(fixed pokemon selection)

--- src/main/java/Computer/Computer.java
+++ src/main/java/Computer/Computer.java
@@ -26,17 +26,26 @@ public abstract class Computer {
         Random rand = new Random();
 
         String[] pokemons = Pokemon.allPokemon;
-        JSONObject[] pokemon = new JSONObject[16];
-        for (int i = 0; i < 16; i++) {
-            int randomNumb = rand.nextInt(0,pokemons.length-1);
-            System.out.println("Requested: " + pokemons[randomNumb]);
-            pokemon[i] = getJSONObject("https://pokeapi.co/api/v2/pokemon/" + (pokemons[randomNumb]).toLowerCase());
-//            System.out.println("Recieved: " + pokemon.get("name"));
+        JSONObject[][] pokemon = new JSONObject[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int randomNumb = rand.nextInt(0,pokemons.length-1);
+//                System.out.println("Requested: " + pokemons[randomNumb]);
+                pokemon[i][j] = getJSONObject("https://pokeapi.co/api/v2/pokemon/" + (pokemons[randomNumb]).toLowerCase());
+            }
         }
+
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                JButton pokebutton = new JButton("" + pokemon[j+i].get("name"));
+//                JButton pokebutton = new JButton("" + pokemon[i][j].get("name"));
+
+                JSONObject sprites = (JSONObject) pokemon[i][j].get("sprites");
+                System.out.println(sprites.get("front_default"));
+
+                ImageIcon img = new ImageIcon("" + sprites.get("front_default"));
+
+                JButton pokebutton = new JButton(img);
                 pokebutton.setBounds((j+1) * 100 + 50, (i+1) * 100 + 50, 100,100);
                 contentpane.add(pokebutton);
             }
